cf61d5c126ae367dc9a3536be36da09f28f2680d
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-06-26 19:36:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+image="";
+objects=[];
+status="";
+
+function preload(){
+    image=loadImage("baby.jpg");
+}
+
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: object detecting";
+}
+function modelLoaded(){
+    console.log('model is loaded');
+    status=true;
+    objectDetector.detect(image, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
+function draw(){
+    image(image,0,0,400,400);
+    if(status != ""){
+        r= random(255);
+        g= random(255);
+        b= random(255);
+        for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="Status: Object Detect";   
+        document.getElementById("baby").innerHTML="Baby found";
+        fill("r,g,b");
+        percent=floor(objects[i].confidence * 100);
+        text(objects[i].label+ " " +percent + "%",objects[i].x + 10,objects[i].y + 20);
+        noFill();
+        stroke("r,g,b");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
